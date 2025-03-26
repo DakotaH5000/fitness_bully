a2730030c4906e2823d3420d28757aa9e28e69d3
DakotaH5000 <DakotaH5000@gmail.com>
2025-03-26 14:02:13 -0400

    First Commit

---
+++ fitness_bully/src/app/header/headerElement.tsx
@@ -0,0 +1,12 @@
+import React, { JSX } from "react";
+import { FC } from 'react';
+
+const HeaderContainer: FC = (): JSX.Element => {
+    return (
+        <div>
+            <p>text</p>
+        </div>
+    );
+};
+
+export default HeaderContainer;
---
+++ fitness_bully/src/app/page.tsx
@@ -0,0 +1,17 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+import HeaderContainer from "./header/headerElement";
+
+export default function Home() {
+  return (
+    <div className={styles.page}>
+      
+      <main className={styles.main}>
+
+      </main>
+      <footer className={styles.footer}>
+
+      </footer>
+    </div>
+  );
+}
